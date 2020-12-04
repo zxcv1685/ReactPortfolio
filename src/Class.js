46e93a61229115bd80af36206ff76529c23aad8e
zxcv1685 <69834166+zxcv1685@users.noreply.github.com>
2020-12-04 16:30:44 +0900

    first commit

---
+++ src/Class.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Class(){
+    return (
+        <div>
+            <h3>class</h3>
+            <div>클래스 컴퍼넌트 추가했음</div>
+        </div>
+    )
+}
+
+export default Class;
---
+++ src/Web.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Web(){
+    return <h2>Web 컴퍼넌트</h2>
+}
+
+export default Web;
